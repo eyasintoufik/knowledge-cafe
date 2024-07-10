caba63cc4faaab8927d9d5b461937ada24fea5f2
unknown <eyasinbhui@gmail.com>
2024-07-11 02:34:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>     
+      <h1 className='text-3xl bg-red-700 text-center m-auto p-3 mt-0'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
